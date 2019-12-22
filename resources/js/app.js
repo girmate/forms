76cfe2ab8f4510c202f683a15f9f21b15db3553c
girmate <girmate@yandex.ru>
2019-12-22 05:28:06 +0300

    enable EventBus

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,8 +20,8 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 //Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('form-select-component', require('./components/FormSelectComponent.vue').default);
 Vue.component('form-component', require('./components/FormComponent.vue').default);
+Vue.component('form-select-component', require('./components/FormSelectComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
